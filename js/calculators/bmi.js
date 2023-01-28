30fcfa646c69cb5e3f85404299c3cb4c1a6733f7
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-01-28 13:35:58 -0600

    Working on bmi

--- js/calculators/bmi.js
+++ js/calculators/bmi.js
@@ -1,21 +1,5 @@
 // // const fetch = require('node-fetch');
 
-// const bmiURL = 'https://body-mass-index-bmi-calculator.p.rapidapi.com/imperial?weight=150&height=68';
-
-// const bmi = {
-//   method: 'GET',
-//   headers: {
-//     'X-RapidAPI-Key': '06ed075c2bmsh028e8aca739c630p1050c5jsn0053a426db18',
-//     'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
-//   }
-// };
-
-// fetch(bmiURL, bmi)
-// 	.then(res => res.json())
-// 	.then(json => console.log(json))
-// 	.catch(err => console.error('error:' + err));
-
-
 
 //BMI Calculator
 function bmiCalc() {
@@ -41,8 +25,10 @@ function bmiCalc() {
           `https://fitness-calculator.p.rapidapi.com/bmi?age=${age}&weight=${weight}&height=${height}`,
           method: "GET",
           headers: {
+            // 'X-RapidAPI-Key': '06ed075c2bmsh028e8aca739c630p1050c5jsn0053a426db18',
+            // 'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
             'X-RapidAPI-Key': '06ed075c2bmsh028e8aca739c630p1050c5jsn0053a426db18',
-            'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
+            'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
           }
       }
       
